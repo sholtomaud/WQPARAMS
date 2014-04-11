907c99b1a81252cd450bd474973b9b2ef4b7f67f
sholto maud <sholto.maud@gmail.com>
2014-04-11 15:00:09 +1000

    added mongo schema

---
+++ server/database/schema/WQschema.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var tenderSchema = new mongoose.Schema({
+  parameter_name: String,
+  cas_no: String,
+  units : String,
+  accuracy : Number,
+  precision : String,
+  dimensions : String,
+  agency_mappings: [{ 
+      agency: String, 
+      agency_last_update: Date, 
+      agency_parameter_name : String, 
+      agency_variable_number : Number, 
+      agency_unit_code : String,
+      agency_units : String,
+      agency_shortname : String,
+      agency_commnt : String,
+      agency_minimum : String,
+      agency_maximum : String,
+      agency_accuracy : Number,
+      agency_precision : Number,
+      agency_dimensions : String
+    }],
+  last_update: Date,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  tags : [String]
+});
+
+module.exports = mongoose.model('Tender', tenderSchema);
